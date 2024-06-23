a98e16ba940a757bf72e35e9c90addd31c811d64
toastoffee <931391442@qq.com>
2024-06-23 21:02:56 +0800

    add string serializer

---
+++ string_serializer.h
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : string_serializer.h
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/6/23
+  ******************************************************************************
+  */
+
+
+
+#ifndef SERIALIZATION_TOOLS_STRING_SERIALIZER_H
+#define SERIALIZATION_TOOLS_STRING_SERIALIZER_H
+
+
+class StringSerializer {
+
+};
+
+
+#endif //SERIALIZATION_TOOLS_STRING_SERIALIZER_H
