82ed49a46b4778a12db8c2161d680da3180991c7
toastoffee <931391442@qq.com>
2024-06-25 12:10:56 +0800

    add buffer class

---
+++ src/buffer.h
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : buffer.h
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/6/25
+  ******************************************************************************
+  */
+
+
+
+#ifndef SERIALIZATION_TOOLS_BUFFER_H
+#define SERIALIZATION_TOOLS_BUFFER_H
+
+
+class buffer {
+
+};
+
+
+#endif //SERIALIZATION_TOOLS_BUFFER_H
