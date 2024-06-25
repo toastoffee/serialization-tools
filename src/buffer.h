8d833c5da71cb59704549dc2ea309932b9799f4b
toastoffee <931391442@qq.com>
2024-06-25 12:43:34 +0800

    add declaration

--- src/buffer.h
+++ src/buffer.h
@@ -13,10 +13,35 @@
 #ifndef SERIALIZATION_TOOLS_BUFFER_H
 #define SERIALIZATION_TOOLS_BUFFER_H
 
+typedef unsigned char Byte;
 
-class buffer {
+#define DEFAULT_BUFFER_MAX_SIZE 1024
+
+class Buffer {
+    explicit Buffer(int bufferSize = DEFAULT_BUFFER_MAX_SIZE);
+    ~Buffer();
+
+    // copy constructor and assignment
+    Buffer(const Buffer& other);
+    Buffer& operator=(const Buffer& other);
+
+    // move constructor and assignment
+    Buffer(Buffer&& other);
+    Buffer& operator=(Buffer&& other);
+    
+private:
+    Byte *data;
+
 
 };
+
+Buffer& Buffer::operator=(const Buffer &other) {
+
+}
+
+Buffer& Buffer::operator=(Buffer &&other) {
+
+}
 
 
 #endif //SERIALIZATION_TOOLS_BUFFER_H
