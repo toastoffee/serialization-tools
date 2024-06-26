3cd0cb8c444a2a7f56c681f4bd1edd2fbb6a3045
toastoffee <931391442@qq.com>
2024-06-26 12:19:06 +0800

    discard string serializer

--- main.cpp
+++ main.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
 #include <utility>
 
-#include "string_serializer.h"
 #include "src/buffer.hpp"
 #include "src/type_helper.h"
 #include "src/byte_serializer.hpp"
-
-struct A{
-    int a;
-    double b;
-    float c;
-
-    char* Serialize(){
-        StringSerializer ss;
-        ss << a << b << c;
-        char *ret;
-        ret = ss.GetString();
-        return ret;
-    }
-
-    void UnSerialize(const char *s){
-        StringSerializer ss(s, sizeof(A));
-        ss >> a >> b >> c;
-    }
-};
 
 struct B{
     int a;
@@ -48,17 +28,6 @@ struct B{
 };
 
 int main() {
-//    A a;
-//    a.a = 10;
-//    a.b = 20.2;
-//    a.c = 30.3;
-//    char *s = a.Serialize();
-//
-//
-//    A b;
-//    b.UnSerialize(s);
-//
-//    std::cout << b.a << " " << b.b << " " << b.c << std::endl;
 
     B test1;
     test1.a = 10;
